0be568392b26fe0b696d45fb71c34a625c094c59
Raul Villarreal <raulevillar30@gmail.com>
2026-05-05 20:15:48 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'acordate',
+  webDir: 'dist'
+};
+
+export default config;
