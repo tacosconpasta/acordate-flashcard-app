6303b3bb08a35d7ca5043585aa8aba678f0597be
Raul Villarreal <raulevillar30@gmail.com>
2026-05-05 22:49:20 -0500

    Batman (Initial Commit)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,40 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import { Redirect, Route } from "react-router-dom";
+import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import Home from "./pages/Home";
+
+//CSS Imports
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+// @ts-ignore: side-effect import of CSS without type declarations
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+// @ts-ignore: side-effect import of CSS without type declarations
+import "@ionic/react/css/normalize.css";
+// @ts-ignore: side-effect import of CSS without type declarations
+import "@ionic/react/css/structure.css";
+// @ts-ignore: side-effect import of CSS without type declarations
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+// @ts-ignore: side-effect import of CSS without type declarations
+import "@ionic/react/css/padding.css";
+
+// @ts-ignore: side-effect import of CSS without type declarations
+import "@ionic/react/css/float-elements.css";
+
+// @ts-ignore: side-effect import of CSS without type declarations
+import "@ionic/react/css/text-alignment.css";
+
+// @ts-ignore: side-effect import of CSS without type declarations
+import "@ionic/react/css/text-transformation.css";
+
+// @ts-ignore: side-effect import of CSS without type declarations
+import "@ionic/react/css/flex-utils.css";
+
+// @ts-ignore: side-effect import of CSS without type declarations
+import "@ionic/react/css/display.css";
 
 /**
  * Ionic Dark Mode
@@ -28,10 +45,12 @@ import '@ionic/react/css/display.css';
 
 /* import '@ionic/react/css/palettes/dark.always.css'; */
 /* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
+// @ts-ignore: side-effect import of CSS without type declarations
+import "@ionic/react/css/palettes/dark.system.css";
 
 /* Theme variables */
-import './theme/variables.css';
+// @ts-ignore: side-effect import of CSS without type declarations
+import "./theme/variables.css";
 
 setupIonicReact();
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,24 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Hello</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Hello World</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
